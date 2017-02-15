99b73eaa0da375e3ca3a69bebf8f00a8416d2f8f
Yasemin Varlik <varly065@newschool.edu>
2017-02-14 23:38:21 -0500

    add javascript

--- space/final/assets/js/app.js
+++ space/final/assets/js/app.js
@@ -7,7 +7,7 @@ $("#rectangle").hover(function() {
 });
 
 $(".rectangles,.rectangles_one,.rectangles_two,.rectangles_three,.rectangles_four,.rectangles_five,.rectangles_six,.rectangles_seven,.rectangles_eight,.rectangles_nine,.rectangles_ten,.rectangles_eleven,.rectangles_twelve,.rectangles_thirteen,.rectangles_fourteen,.rectangles_fifthteen,.rectangles_sixteen").click(function() {
-	console.log("hello");
+	console.log();
 	$(this).css("background-color","#4F99D4");
 
 
