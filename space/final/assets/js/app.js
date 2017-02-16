ef5d985e2950ae76cf5ee6ef8571a855bd40f3fc
Yasemin Varlik <varly065@newschool.edu>
2017-02-16 08:59:45 -0500

    add generic rectangle class to target, move hover code to css

--- space/final/assets/js/app.js
+++ space/final/assets/js/app.js
@@ -1,14 +1,6 @@
 
-console.log();
-$("#rectangle").hover(function() {
-    $(this).css('cursor','pointer');
-}, function() {
-    $(this).css('cursor','auto');
-});
+console.log("test");
 
-$(".rectangles,.rectangles_one,.rectangles_two,.rectangles_three,.rectangles_four,.rectangles_five,.rectangles_six,.rectangles_seven,.rectangles_eight,.rectangles_nine,.rectangles_ten,.rectangles_eleven,.rectangles_twelve,.rectangles_thirteen,.rectangles_fourteen,.rectangles_fifthteen,.rectangles_sixteen").click(function() {
-	console.log();
+$(".rectangle").click(function() {
 	$(this).css("background-color","#4F99D4");
-
-
 })
